c970a265a585ad33e5603e1fe96bf52b641a0012
joao <joao.kz2004@aluno.ifsc.edu.br>
2022-08-18 11:03:29 -0300

    Classe Livro

---
+++ src/modelo/Livro.java
@@ -0,0 +1,69 @@
+package modelo;
+
+public class Livro {
+	private float preco;
+	private String editora;
+	private int ano;
+	private String titulo;
+	private int codigo;
+	private String idioma;
+	private String genero;
+	private int nPaginas;
+	Autor autor;
+	
+	public Autor getAutor() {
+		return autor;
+	}
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getIdioma() {
+		return idioma;
+	}
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public int getnPaginas() {
+		return nPaginas;
+	}
+	public void setnPaginas(int nPaginas) {
+		this.nPaginas = nPaginas;
+	}
+
+}
