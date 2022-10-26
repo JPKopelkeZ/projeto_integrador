eaba99a3fd67a08b784f5b7164fc14a64bd130e2
Maria <maria.isabelbase@gmail.com>
2022-10-26 16:53:37 -0300

    Tela Histórico de preço e comandos feitos.

--- src/visao/TelaHistoricodePreco.java
+++ src/visao/TelaHistoricodePreco.java
@@ -13,11 +13,17 @@ import javax.swing.JButton;
 import java.awt.SystemColor;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaHistoricodePreco extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -39,6 +45,8 @@ public class TelaHistoricodePreco extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaHistoricodePreco() {
+		setMaximumSize(new Dimension(963, 603));
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 963, 603);
 		contentPane = new JPanel();
@@ -61,28 +69,36 @@ public class TelaHistoricodePreco extends JFrame {
 		panel.add(lblNomeEmpresa);
 		
 		JButton btnVoltar = new JButton(" Voltar");
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+			
+			
+			
+		});
 		btnVoltar.setFont(new Font("Bookman Old Style", Font.PLAIN, 14));
 		btnVoltar.setBackground(SystemColor.menu);
-		btnVoltar.setBounds(26, 62, 106, 32);
+		btnVoltar.setBounds(26, 54, 106, 32);
 		panel.add(btnVoltar);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(57, 209, 81, -65);
-		contentPane.add(scrollPane);
-		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(10, 116, 927, 424);
+		panel_1.setBounds(28, 116, 891, 419);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
-		JLabel Nome = new JLabel("Nome ");
-		Nome.setFont(new Font("Bookman Old Style", Font.PLAIN, 16));
-		Nome.setBounds(42, 11, 114, 25);
-		panel_1.add(Nome);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 891, 419);
+		panel_1.add(scrollPane);
 		
-		textField = new JTextField();
-		textField.setBounds(93, 10, 685, 31);
-		panel_1.add(textField);
-		textField.setColumns(10);
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Nome", "N.Supervisor", "D.Altera\u00E7\u00E3o", "P.Anterior", "P.Alterado"
+			}
+		));
+		scrollPane.setViewportView(table);
 	}
 }
