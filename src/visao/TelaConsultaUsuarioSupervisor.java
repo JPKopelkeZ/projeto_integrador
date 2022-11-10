f28d4b984a141a601de78c9634ec2f466caba3de
Maria Isabel Medina Base <maria.isabelbase@gmail.com>
2022-11-10 11:52:02 -0300

    Funcionamento das telas

--- src/visao/TelaConsultaUsuarioSupervisor.java
+++ src/visao/TelaConsultaUsuarioSupervisor.java
@@ -6,43 +6,54 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.border.LineBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.SystemColor;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import controle.FornecedorBD;
+import controle.UsuarioBD;
+import modelo.Endereco;
+import modelo.Fornecedor;
+import modelo.Usuario;
+
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
 public class TelaConsultaUsuarioSupervisor extends JFrame {
 	private JTable table;
+	private JPanel contentPane;
+	ArrayList<Usuario> listaUsuario = new ArrayList<Usuario>();
+	UsuarioBD bd = new UsuarioBD();
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TelaConsultaUsuarioSupervisor frame = new TelaConsultaUsuarioSupervisor();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 */
-	public TelaConsultaUsuarioSupervisor() {
-		getContentPane().setBackground(new Color(153, 204, 255));
-		getContentPane().setForeground(new Color(102, 204, 255));
-		getContentPane().setLayout(null);
+	public TelaConsultaUsuarioSupervisor(Usuario usuario ) {
+		listaUsuario = bd.mostrarUsuarios();
+		setMaximumSize(new Dimension(963, 603));
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 963, 603);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(153, 204, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
@@ -59,6 +70,9 @@ public class TelaConsultaUsuarioSupervisor extends JFrame {
 		JButton btnVoltar = new JButton(" Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+				TelaInicialSupervisor telaSupervisor = new TelaInicialSupervisor(usuario);
+				telaSupervisor.setVisible(true);
 			}
 		});
 		btnVoltar.setFont(new Font("Bookman Old Style", Font.PLAIN, 14));
@@ -71,16 +85,29 @@ public class TelaConsultaUsuarioSupervisor extends JFrame {
 		getContentPane().add(scrollPane);
 		
 		table = new JTable();
-		table.setModel(new DefaultTableModel(
+		
+		
+		DefaultTableModel model = new DefaultTableModel(
 			new Object[][] {
 			},
 			new String[] {
-				"Nome do usu\u00E1rio", "Fun\u00E7\u00E3o", "ID"
+				"Senha", "Nome do usu\u00E1rio", "Fun\u00E7\u00E3o", "ID"
 			}
-		));
+		);
+		for (int i = 0; i < listaUsuario.size(); i++) {
+			Usuario f = listaUsuario.get(i);
+			
+			model.addRow(new Object[] {f.getSenha(), f.getEmail(), f.getTipoUsuario(), f.getId_funcionario()});
+		}
+		table.setModel(model);
+		
 		scrollPane.setViewportView(table);
 		
 		JButton btnSelecionar = new JButton("Selecionar");
+		btnSelecionar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnSelecionar.setForeground(Color.BLACK);
 		btnSelecionar.setFont(new Font("Bookman Old Style", Font.PLAIN, 12));
 		btnSelecionar.setBackground(SystemColor.menu);
@@ -88,6 +115,21 @@ public class TelaConsultaUsuarioSupervisor extends JFrame {
 		getContentPane().add(btnSelecionar);
 		
 		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				Usuario c = listaUsuario.get(table.getSelectedRow());
+				if (c != null) {
+					bd.excluirUsuario(c);
+					listaUsuario.remove(c);
+					JOptionPane.showMessageDialog(null, "Usuário excluído com sucesso!");
+					
+					setVisible(false);
+					TelaConsultaUsuarioSupervisor consultaUsuario = new TelaConsultaUsuarioSupervisor(usuario);
+					consultaUsuario.setVisible(true);
+				}
+			}
+		});
 		btnExcluir.setForeground(Color.BLACK);
 		btnExcluir.setFont(new Font("Bookman Old Style", Font.PLAIN, 12));
 		btnExcluir.setBackground(SystemColor.menu);
